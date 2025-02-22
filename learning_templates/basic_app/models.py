27a5b00a616e061ece7fcf1ab0589c9fa6bdb87a
TharwatMella <tharwat-mella@hotmail.com>
2025-02-22 03:24:16 +0300

    modified

--- learning_templates/basic_app/models.py
+++ learning_templates/basic_app/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+
 class NameModel(models.Model):
-    name_field=models.TextField(max_length=20)
+    name_field = models.TextField(max_length=20)
+
     def __str__(self):
         return self.name_field
-    
